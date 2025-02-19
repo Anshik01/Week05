083bcf43a970cbe75d0b1d0ad680d5b02bb04948
Anshik01 <anshik0706@gmail.com>
2025-02-19 12:56:02 +0530

    [Anshik Jaiswal] Add. Read json file

---
+++ Day02/src/main/com/readjson/ReadJsonFile.java
@@ -0,0 +1,49 @@
+package com.readjson;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+class Student{
+    private String name;
+    private String email;
+    private int marks;
+
+    public Student() {}
+
+    public Student(String name, String type, int price){
+        this.name = name;
+        this.email = type;
+        this.marks = price;
+    }
+
+    // getter methods to access private fields
+    public String getName(){
+        return name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public int getMarks(){
+        return marks;
+    }
+}
+
+public class ReadJsonFile {
+    public static void main(String[] args) {
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // reading json file using object mapper
+            Student student = objectMapper.readValue(new File("data.json"), Student.class);
+
+            // extracting only required field
+            System.out.println("Name : " + student.getName());
+            System.out.println("Email : " + student.getEmail());
+
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
