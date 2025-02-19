8d977a4b7a220b754e72e9e7dc0de863e7c8a433
Anshik01 <anshik0706@gmail.com>
2025-02-19 12:03:03 +0530

    [Anshik Jaiswal] Add. create json object

---
+++ Day02/src/main/com/jsonobject/CreateJasonObject.java
@@ -0,0 +1,26 @@
+package com.jsonobject;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class CreateJasonObject {
+    public static void main(String[] args) {
+        // creating JSONArray to store subjects
+        JSONArray subjects = new JSONArray();
+
+        subjects.put("Physics");
+        subjects.put("Chemistry");
+        subjects.put("Math");
+
+        JSONObject student = new JSONObject();
+
+        // JSONObject store name, age and subject array
+        student.put("Name", "Anshik Jaiswal");
+        student.put("Age", 20);
+        student.put("Subjects", subjects);
+
+
+        System.out.println("Student data: ");
+        System.out.println(student);
+    }
+}
